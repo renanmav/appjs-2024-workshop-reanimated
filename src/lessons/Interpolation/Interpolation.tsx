05eab4ad787e351f9a4d6da19e149fb01e358ee9
Renan Machado <renan.machado@coinbase.com>
2024-07-06 21:47:22 -0300

    Finish interpolation lesson

--- src/lessons/Interpolation/Interpolation.tsx
+++ src/lessons/Interpolation/Interpolation.tsx
@@ -1,21 +1,35 @@
+import { AnimatedText } from "@/components/AnimatedText";
 import { Container } from "@/components/Container";
 import { items } from "@/lib/mock";
 import { colors, layout } from "@/lib/theme";
 import React from "react";
-import {
-  FlatList,
-  ListRenderItemInfo,
-  StyleSheet,
-  Text,
-  View,
-} from "react-native";
+import { ListRenderItemInfo, StyleSheet, Text } from "react-native";
+import Animated, {
+  interpolate,
+  interpolateColor,
+  SharedValue,
+  useAnimatedScrollHandler,
+  useAnimatedStyle,
+  useSharedValue,
+} from "react-native-reanimated";
 
 type ItemType = (typeof items)[0];
 
 export function Interpolation() {
+  const scrollX = useSharedValue(0);
+
+  const onScroll = useAnimatedScrollHandler((event) => {
+    scrollX.value = Number(
+      (
+        event.contentOffset.x /
+        (layout.itemSize + layout.spacing)
+      ).toExponential(1)
+    );
+  });
+
   return (
     <Container style={styles.container}>
-      <FlatList
+      <Animated.FlatList
         data={items}
         horizontal
         contentContainerStyle={{
@@ -31,19 +45,48 @@ export function Interpolation() {
         snapToInterval={layout.itemSize + layout.spacing}
         // This is to snap faster to the closest item
         decelerationRate={"fast"}
-        renderItem={(props) => <Item {...props} />}
+        renderItem={(props) => <Item {...props} scrollX={scrollX} />}
+        onScroll={onScroll}
+        scrollEventThrottle={1000 / 60}
       />
     </Container>
   );
 }
 
-type ItemProps = ListRenderItemInfo<ItemType> & {};
+type ItemProps = ListRenderItemInfo<ItemType> & {
+  scrollX: SharedValue<number>;
+};
 
-export function Item({ item, index }: ItemProps) {
+export function Item({ item, index, scrollX }: ItemProps) {
+  const stylez = useAnimatedStyle(() => {
+    return {
+      opacity: interpolate(
+        scrollX.value,
+        [index - 1, index, index + 1],
+        [0.75, 1, 0.75]
+      ),
+      backgroundColor: interpolateColor(
+        scrollX.value,
+        [index - 1, index, index + 1],
+        [colors.purple, colors.overlay, colors.green]
+      ),
+      transform: [
+        {
+          scale: interpolate(
+            scrollX.value,
+            [index - 1, index, index + 1],
+            [0.9, 1, 0.9]
+          ),
+        },
+      ],
+    };
+  });
+
   return (
-    <View style={styles.item}>
+    <Animated.View style={[styles.item, stylez]}>
       <Text>{item.label}</Text>
-    </View>
+      <AnimatedText text={scrollX} label="offset: " />
+    </Animated.View>
   );
 }
 
